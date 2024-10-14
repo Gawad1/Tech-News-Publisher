ba8c9704d07690b291199c0fb7b523ed7bf02cba
Gawad1 <gawad.hussein@gmail.com>
2024-10-14 14:42:38 +0300

    action build

--- download_models.py
+++ download_models.py
@@ -1,8 +1,10 @@
 # download_models.py
 from transformers import pipeline, BartTokenizer
 from keybert import KeyBERT
+import os
 
 # Pre-download the tokenizer and model
+os.environ["HF_HOME"] = "/app/huggingface_cache"
 kw_model = KeyBERT()
 tokenizer = BartTokenizer.from_pretrained("facebook/bart-large-cnn")
 summarizer = pipeline("summarization", model="facebook/bart-large-cnn")
